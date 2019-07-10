b05e102a571889994f05b6c58345696c32145ac1
Shenpanpan123 <m18040030328@qq.com>
2019-07-10 14:37:03 +0800

    将master分支上的say方法hello git 修改为hello maven

--- day2exam/src/main/java/com/bawei/HelloGit.java
+++ day2exam/src/main/java/com/bawei/HelloGit.java
@@ -3,7 +3,7 @@ package com.bawei;
 public class HelloGit {
 	
 	public void say(){
-		System.out.println("hello git");
+		System.out.println("hello maven");
 		System.out.println("hello 1703D");
 	}
 	
